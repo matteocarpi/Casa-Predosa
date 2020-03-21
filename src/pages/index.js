a95b0964f0469cc62f2dc7fe6bf6c553bac9f590
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-21 09:02:13 +0100

    chore: linted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import styles from '../styles/Home.module.scss';
+import { graphql } from 'gatsby';
 
 const IndexPage = () => (
   <Layout>
@@ -9,11 +11,13 @@ const IndexPage = () => (
       className={styles.titleSlide}
       >
         <div className={styles.title}>
-          <h1 className={universal.white}>Casa Predosa</h1>
+          <h1 className='white'>Casa Predosa</h1>
         </div>
 
       </div>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
+
+
